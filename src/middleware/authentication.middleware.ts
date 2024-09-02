d90559eddc5dbc8ba80523b7cd644be1efc4bbeb
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-09-02 22:57:28 +0300

    add authentication middleware

---
+++ src/middleware/authentication.middleware.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+import UserModel from '../models/user.model';
+import config from '../config';
+import Error from '../interfaces/error.interface';
+
+const handelUnAuthorized = (next: NextFunction) => {
+  const error: Error = new Error('Login error: please try again');
+  error.status = 401;
+  next(error);
+};
+
+const validateTokenMiddleware = (
+  req: Request,
+  _res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const authHeader = req.get('Authorization');
+    console.log(authHeader);
+    if (authHeader) {
+      const bearer = authHeader.split(' ')[0].toLowerCase();
+      const token = authHeader.split(' ')[1];
+      if (token && bearer === 'bearer') {
+        jwt.verify(
+          token,
+          config.tokenSecret as unknown as string,
+          (err, decoded) => {
+            if (err) {
+              handelUnAuthorized(next);
+            } else {
+              next();
+            }
+          },
+        );
+      }
+    }
+  } catch (error) {
+    handelUnAuthorized(next);
+  }
+};
+
+export default validateTokenMiddleware;
