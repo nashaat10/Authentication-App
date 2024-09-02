0b3d86cddcc0bd18dd495170b8624796a930e3c2
nashaat10 <94199891+nashaat10@users.noreply.github.com>
2024-09-02 22:58:28 +0300

    add authentication routes

--- src/routes/api/users.routes.ts
+++ src/routes/api/users.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import * as controllers from '../../controllers/users.controllers';
+import authenticationMiddleware from '../../middleware/authentication.middleware';
 
 const routes = Router();
 
-routes.route('/').get(controllers.getMany).post(controllers.create);
+routes
+  .route('/')
+  .get(authenticationMiddleware, controllers.getMany)
+  .post(controllers.create);
 routes
   .route('/:id')
   .get(controllers.getOne)
